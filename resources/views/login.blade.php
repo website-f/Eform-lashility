7e839274c5d0ea6db287dbcb3fbad9c334d74730
website-f <mohdfitri1017@gmail.com>
2023-08-29 10:57:53 +0800

    login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -50,7 +50,8 @@
               <div class="card" style="border-radius: 1rem;">
                 <div class="row g-0">
                   <div class="col-md-6 col-lg-5 d-none d-md-block loginImageView">
-                    
+                    <img src="{{asset('images/Asset-7.png')}}"
+                      alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem; height: 630px;" width="800" />
                   </div>
                   <div class="col-md-6 col-lg-7 d-flex align-items-center">
                     <div class="card-body p-4 p-lg-5 text-black">
