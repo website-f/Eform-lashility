b55df03bce215abc4f0b4e7e568b15e7b93614e4
website-f <mohdfitri1017@gmail.com>
2023-08-30 15:14:03 +0800

    edit

--- resources/views/myform.blade.php
+++ resources/views/myform.blade.php
@@ -1,6 +1,6 @@
 @extends('partial.main')
 @section('title', 'MyForms')
-    
+
 @section('content')
 
 
@@ -27,13 +27,13 @@
               <h5 class=""><a href="/form-view/{{$forms->id}}"><b>{{$forms->type}} Form</b></a></h5>
               <span style="font-size: 12px">{{$submittedCount}} Submission, Updated on {{$forms['updated_at']->format('d-m-Y')}}</span>
           </div>
-      
+
           <nav class="header-nav ms-auto">
             <ul class="d-flex align-items-center">
               @if (Auth::user()->role->role == "SuperAdmin" || Auth::user()->role->role == "Admin")
               <li class="nav-item dropdown p-2">
-                  
-                
+
+
                 @if ($forms->approval == "pending")
                 @php
                 $approvalCount = $submitted->where('type', $forms->type.' Form')->where('approval', 'pending')->count();
@@ -42,13 +42,13 @@
                       Need Approval <span class="badge bg-secondary text-light">{{$approvalCount}}</span>
                     </a>
                 @endif
-          
-          
+
+
               </li><!-- End Messages Nav -->
-          
+
               <li class="nav-item dropdown p-2">
                 <!-- Vertically centered Modal -->
-               
+
                 <a type="button" class="btn btn-outline-danger btn-sm" data-bs-toggle="modal" data-bs-target="#verticalycentered-${{$forms->id}}">
                   <i class="bi bi-trash3-fill"></i>
                 </a>
@@ -70,56 +70,58 @@
                   </div>
                 </div><!-- End Vertically centered Modal-->
                 @endif
-                
-          
+
+
               </li><!-- End Notification Nav -->
-          
+
               <li class="nav-item dropdown p-2">
                 <a class="btn btn-outline-primary btn-sm" href="/submitted-based/{{$formType}}">
                   <i class="bi bi-envelope-exclamation-fill"></i>
                  Inbox
                 </a><!-- End Messages Icon -->
-          
-          
+
+
               </li><!-- End Messages Nav -->
               @if (Auth::user()->role->role == "SuperAdmin" || $forms->user->name == Auth::user()->name || Auth::user()->role->role == "Admin")
+
+              @if ($submittedCount > 0)
+
+              @else
               <li class="nav-item dropdown p-1">
-               
                 <a class="btn btn-outline-primary btn-sm" href="/edit-view-form/{{$forms->id}}" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit">
                   <i class="bi bi-pencil-square"></i>
                 </a><!-- End Messages Icon -->
-          
-          
               </li><!-- End Messages Nav -->
               @endif
+              @endif
               <li class="nav-item dropdown p-1">
-               
+
                 <a class="btn btn-outline-secondary btn-sm" href="/form-view/{{$forms->id}}" data-bs-toggle="tooltip" data-bs-placement="top" title="View">
                   <i class="bi bi-eye-fill"></i>
                 </a><!-- End Messages Icon -->
-          
-          
+
+
               </li><!-- End Messages Nav -->
               <li class="nav-item dropdown p-1">
-               
+
                 <a class="btn btn-outline-warning btn-sm" href="/form-clone/{{$forms->id}}" data-bs-toggle="tooltip" data-bs-placement="top" title="Clone">
                   <i class="bi bi-clipboard-fill"></i>
                 </a><!-- End Messages Icon -->
-          
-          
+
+
               </li><!-- End Messages Nav -->
-          
+
             </ul>
           </nav><!-- End Icons Navigation -->
-          
-        
+
+
         </div><!-- End Header -->
-        
+
       @endforeach
     </div>
     <div style="margin-top: 10px">
       {{ $form->links() }}
     </div>
-    
+
   </section>
 @endsection
